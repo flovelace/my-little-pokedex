01dd5a334f92104f233ec66e4f6a30de53a98f44
flovelace <lovelace.af@gmail.com>
2023-01-29 15:55:43 -0500

    added functions to account for user input such as capitalisation

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
+// Select search field
 document.querySelector('#search').addEventListener('click', getPokemon);
 
+// function to convert user input to lowercase
+function lowerCaseName(string) {
+  return string.toLowerCase();
+}
+
+// Capitalise the first letter on the pokemon name h1
+function capitaliseFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Invoke the function to get the data from the API on click
 function getPokemon(e) {
   const pokemonName = document.querySelector('#pokemonName').value;
+  const lowerCasePokemonName = lowerCaseName(pokemonName);
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+  fetch(`https://pokeapi.co/api/v2/pokemon/${lowerCasePokemonName}`)
     .then((response) => response.json())
     .then((data) => {
       document.querySelector('.pokemonBox').innerHTML = `
           <div>
-            <img src="${data.sprites.other.home.front_default}" alt="${data.pokemonName}" style="height: 500px; width: 500px;">
+            <img src="${
+              data.sprites.other.home.front_default
+            }" alt="${capitaliseFirstLetter(
+        data.name,
+      )}" style="height: 500px; width: 500px;">
           </div>
           <div class="pokemonInfo">
-            <h1>${data.name}</h1>
+            <h1>${capitaliseFirstLetter(data.name)}</h1>
             <p>${data.weight}</p>
           </div>
         </div>
