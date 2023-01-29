d26b6dcb491319478e6dd9b9b788aebf5e7abe71
flovelace <lovelace.af@gmail.com>
2023-01-28 21:24:42 -0500

    successfully implemented api call with pokemon and the picture

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function getPokemon() {
-  fetch('https://pokeapi.co/api/v2/pokemon/charmander').then;
-}
+document.querySelector('#search').addEventListener('click', getPokemon);
 
-getPokemon();
+function getPokemon(e) {
+  const pokemonName = document.querySelector('#pokemonName').value;
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector('.pokemonBox').innerHTML = `
+          <div>
+            <img src="${data.sprites.other.home.front_default}" alt="${data.pokemonName}" style="height: 500px; width: 500px;">
+          </div>
+          <div class="pokemonInfo">
+            <h1>${data.name}</h1>
+            <p>${data.weight}</p>
+          </div>
+        </div>
+      `;
+    })
+    .catch((err) => {
+      console.log('Pokemon not found!', err);
+    });
+
+  e.preventDefault();
+}
