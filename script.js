4543cc9eaa80353d17f154c99d985a301304aa0f
flovelace <lovelace.af@gmail.com>
2023-01-30 22:12:26 -0500

    finished random button function. added some basic stylings

--- script.js
+++ script.js
@@ -55,5 +55,33 @@ function getPokemon(e) {
 }
 
 function luckyPokemon(e) {
+  const randomNumber = Math.ceil(Math.random() * 900);
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector('.pokemonBox').innerHTML = `
+          <div>
+            <img src="${
+              data.sprites.other.home.front_default
+            }" alt="${capitaliseFirstLetter(
+        data.name,
+      )}" style="height: 500px; width: 500px;">
+          </div>
+          <div class="pokemonInfo">
+            <h1>${capitaliseFirstLetter(data.name)}</h1>
+            <h2>Base Stats</h2>
+            <p>HP: ${data.stats[0].base_stat}</p>
+            <p>Attack: ${data.stats[1].base_stat}</p>
+            <p>Defence: ${data.stats[2].base_stat}</p>
+            <p>Special Attack: ${data.stats[3].base_stat}</p>
+            <p>Special Defence: ${data.stats[4].base_stat}</p>
+            <p>Speed: ${data.stats[5].base_stat}</p>
+            <p>Weight: ${data.weight}</p>
+          </div>
+        </div>
+      `;
+    });
+
   e.preventDefault();
 }
