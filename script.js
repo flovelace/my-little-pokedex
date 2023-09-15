6030e074d960149eddf38b312579d406bf23196a
flovelace <lovelace.af@gmail.com>
2023-09-15 17:32:20 -0400

    Refactored codebase with updated search function.

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
 // Select search button
-document.querySelector('#search').addEventListener('click', getPokemon);
+document
+  .querySelector('#search')
+  .addEventListener('click', () => handlePokemonSearch(false));
 
 // Select the lucky button
-document.querySelector('#lucky').addEventListener('click', luckyPokemon);
+document
+  .querySelector('#lucky')
+  .addEventListener('click', () => handlePokemonSearch(true));
 
 const types = document.querySelector('#types');
 
-// function to convert user input to lowercase
+// Function to convert user input to lowercase
 function lowerCaseName(string) {
   return string.toLowerCase();
 }
 
-// Capitalise the first letter on the pokemon name h1
-function capitaliseFirstLetter(string) {
+// Capitalize the first letter of the Pokemon name
+function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 // Invoke the function to get the data from the API on click
-function getPokemon(e) {
-  const pokemonName = document.querySelector('#pokemonName').value;
-  const lowerCasePokemonName = lowerCaseName(pokemonName);
+// Reusable function to fetch and display Pokemon data
+function handlePokemonSearch(isRandom) {
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/${lowerCasePokemonName}`)
+  if (isRandom) {
+    const randomNumber = Math.ceil(Math.random() * 898);
+    apiUrl += randomNumber;
+  } else {
+    const pokemonName = document.querySelector('#pokemonName').value;
+    const lowerCasePokemonName = lowerCaseName(pokemonName);
+    apiUrl += lowerCasePokemonName;
+  }
+
+  fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      // If the pokemon has no types
+      // If the Pokemon has no types
       if (data.types.length === 0) {
         let newType = document.createElement('span');
         newType.innerHTML = 'No type';
@@ -34,13 +47,13 @@ function getPokemon(e) {
 
       document.querySelector('.pokemonBox').innerHTML = `
         <div class="pokemonContainer">
-            <img src="${
-              data.sprites.other.home.front_default
-            }" alt="${capitaliseFirstLetter(
+          <img src="${
+            data.sprites.other.home.front_default
+          }" alt="${capitalizeFirstLetter(
         data.name,
       )}" style="height: 300px; width: 300px;">
           <div class="pokemonInfo">
-            <h2>${capitaliseFirstLetter(data.name)}</h2>
+            <h2>${capitalizeFirstLetter(data.name)}</h2>
             <span class="badge">${data.types[0].type.name}</span>
             ${
               data.types.length > 1
@@ -50,9 +63,9 @@ function getPokemon(e) {
             <h3>Base Stats</h3>
             <p>HP: ${data.stats[0].base_stat}</p>
             <p>Attack: ${data.stats[1].base_stat}</p>
-            <p>Defence: ${data.stats[2].base_stat}</p>
+            <p>Defense: ${data.stats[2].base_stat}</p>
             <p>Special Attack: ${data.stats[3].base_stat}</p>
-            <p>Special Defence: ${data.stats[4].base_stat}</p>
+            <p>Special Defense: ${data.stats[4].base_stat}</p>
             <p>Speed: ${data.stats[5].base_stat}</p>
             <p>Weight: ${data.weight}</p>
           </div>
@@ -61,60 +74,8 @@ function getPokemon(e) {
     })
     .catch((err) => {
       console.log(err);
-      return (document.querySelector(
-        '.pokemonBox',
-      ).innerHTML = `<h1>POKEMANS NOT FOUND</h1>`);
+      document.querySelector('.pokemonBox').innerHTML = isRandom
+        ? `<h1>ERROR! Could not find a random Pokemon</h1>`
+        : `<h1>POKEMON NOT FOUND</h1>`;
     });
-
-  e.preventDefault();
-}
-
-function luckyPokemon(e) {
-  const randomNumber = Math.ceil(Math.random() * 898);
-
-  fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
-    .then((response) => response.json())
-    .then((data) => {
-      // If the pokemon has no types
-      if (data.types.length === 0) {
-        let newType = document.createElement('span');
-        newType.innerHTML = 'No type';
-        newType.classList.add('badge');
-        types.appendChild(newType);
-      }
-      document.querySelector('.pokemonBox').innerHTML = `
-        <div class="pokemonContainer">
-            <img src="${
-              data.sprites.other.home.front_default
-            }" alt="${capitaliseFirstLetter(
-        data.name,
-      )}" style="height: 300px; width: 300px;">
-          <div class="pokemonInfo">
-            <h2>${capitaliseFirstLetter(data.name)}</h2>
-            <span class="badge">${data.types[0].type.name}</span>
-            ${
-              data.types.length > 1
-                ? `<span class="badge">${data.types[1].type.name}</span>`
-                : ''
-            }
-            <h3>Base Stats</h3>
-            <p>HP: ${data.stats[0].base_stat}</p>
-            <p>Attack: ${data.stats[1].base_stat}</p>
-            <p>Defence: ${data.stats[2].base_stat}</p>
-            <p>Special Attack: ${data.stats[3].base_stat}</p>
-            <p>Special Defence: ${data.stats[4].base_stat}</p>
-            <p>Speed: ${data.stats[5].base_stat}</p>
-            <p>Weight: ${data.weight}</p>
-          </div>
-        </div>
-      `;
-    })
-    .catch((err) => {
-      console.log(err);
-      return (document.querySelector(
-        '.pokemonBox',
-      ).innerHTML = `<h1>ERROR! could not find pokemon</h1>`);
-    });
-
-  e.preventDefault();
 }
